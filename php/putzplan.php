8fa9a9c472a4ba191ee9fbf7502a60a5d4058f36
Max <mail@max-werner.net>
2020-01-18 18:45:21 +0100

    update

--- php/putzplan.php
+++ php/putzplan.php
@@ -2,6 +2,12 @@
 	# Load putzplan from JSON
     $str = file_get_contents('../data/putzplan.json');
     $json = json_decode($str, true);
+
+    $bad_vergangen = $json['bad']['tage_vergangen'];
+    $bad_intervall = $json['bad']['intervall_tage'];
+    $bad_verbleibend = $bad_intervall - $bad_vergangen
+    if ($bad_verbleibend > 1)
+        $bad_faellig = "in " + $bad_verbleibend + " Tagen"
 ?>
 
 	<table class="putzplan">
@@ -12,36 +18,36 @@
 		<tr>
 		    <th>Müll rausbringen</th>
 		    <td><?php echo $json['muell']['dran']; ?></td>
-		    <td></td>
+		    <td>bei Bedarf</td>
 		</tr>
 		<tr>
 		    <th>Glas wegbringen</th>
-		    <td></td>
-		    <td></td>
+		    <td><?php echo $json['glas']['dran']; ?></td>
+		    <td>bei Bedarf</td>
 		</tr>
 		<tr>
 		    <th>Bäder putzen</th>
-		    <td></td>
-		    <td></td>
+		    <td><?php echo $json['bad']['dran']; ?></td>
+		    <td><?php echo $bad_faellig; ?></td>
 		</tr>
 		<tr>
 		    <th>Küche putzen</th>
-		    <td></td>
+		    <td><?php echo $json['kueche']['dran']; ?></td>
 		    <td></td>
 		</tr>
 		<tr>
 		    <th>Staubsaugen</th>
-		    <td></td>
+		    <td><?php echo $json['saugen']['dran']; ?></td>
 		    <td></td>
 		</tr>
 		<tr>
 		    <th>Handtücher waschen</th>
-		    <td></td>
+		    <td><?php echo $json['handtuecher']['dran']; ?></td>
 		    <td></td>
 		</tr>
 		<tr>
 		    <th>Duschvorhänge waschen</th>
-		    <td></td>
+		    <td><?php echo $json['duschvorhang']['dran']; ?></td>
 		    <td></td>
 		</tr>
 	</table>
