6243399d3b213c8a0fdad1df54155883cb84ed44
Max <mail@max-werner.net>
2020-01-18 19:29:04 +0100

    finished putzplan php

--- php/putzplan.php
+++ php/putzplan.php
@@ -3,6 +3,7 @@
     $str = file_get_contents('../data/putzplan.json');
     $json = json_decode($str, true);
 
+    # bad
     $bad_vergangen = $json['bad']['tage_vergangen'];
     $bad_intervall = $json['bad']['intervall_tage'];
     $bad_verbleibend = $bad_intervall - $bad_vergangen;
@@ -16,6 +17,66 @@
         $bad_faellig = "<span class='overdue'>gestern</span>";
     else
         $bad_faellig = "<span class='overdue'>seit " . abs($bad_verbleibend) . " Tagen</span>"
+
+    # kueche
+    $kueche_vergangen = $json['kueche']['tage_vergangen'];
+    $kueche_intervall = $json['kueche']['intervall_tage'];
+    $kueche_verbleibend = $kueche_intervall - $kueche_vergangen;
+    if ($kueche_verbleibend > 1)
+        $kueche_faellig = "in " . $kueche_verbleibend . " Tagen";
+    elseif ($kueche_verbleibend == 1)
+        $kueche_faellig = "morgen";
+    elseif ($kueche_verbleibend == 0)
+        $kueche_faellig = "<span class='due'>heute</span>";
+    elseif ($kueche_verbleibend == -1)
+        $kueche_faellig = "<span class='overdue'>gestern</span>";
+    else
+        $kueche_faellig = "<span class='overdue'>seit " . abs($kueche_verbleibend) . " Tagen</span>"
+
+    # saugen
+    $saugen_vergangen = $json['saugen']['tage_vergangen'];
+    $saugen_intervall = $json['saugen']['intervall_tage'];
+    $saugen_verbleibend = $saugen_intervall - $saugen_vergangen;
+    if ($saugen_verbleibend > 1)
+        $saugen_faellig = "in " . $saugen_verbleibend . " Tagen";
+    elseif ($saugen_verbleibend == 1)
+        $saugen_faellig = "morgen";
+    elseif ($saugen_verbleibend == 0)
+        $saugen_faellig = "<span class='due'>heute</span>";
+    elseif ($saugen_verbleibend == -1)
+        $saugen_faellig = "<span class='overdue'>gestern</span>";
+    else
+        $saugen_faellig = "<span class='overdue'>seit " . abs($saugen_verbleibend) . " Tagen</span>"
+
+    # handtuecher
+    $handtuecher_vergangen = $json['handtuecher']['tage_vergangen'];
+    $handtuecher_intervall = $json['handtuecher']['intervall_tage'];
+    $handtuecher_verbleibend = $handtuecher_intervall - $handtuecher_vergangen;
+    if ($handtuecher_verbleibend > 1)
+        $handtuecher_faellig = "in " . $handtuecher_verbleibend . " Tagen";
+    elseif ($handtuecher_verbleibend == 1)
+        $handtuecher_faellig = "morgen";
+    elseif ($handtuecher_verbleibend == 0)
+        $handtuecher_faellig = "<span class='due'>heute</span>";
+    elseif ($handtuecher_verbleibend == -1)
+        $handtuecher_faellig = "<span class='overdue'>gestern</span>";
+    else
+        $handtuecher_faellig = "<span class='overdue'>seit " . abs($handtuecher_verbleibend) . " Tagen</span>"
+
+    # duschvorhang
+    $duschvorhang_vergangen = $json['duschvorhang']['tage_vergangen'];
+    $duschvorhang_intervall = $json['duschvorhang']['intervall_tage'];
+    $duschvorhang_verbleibend = $duschvorhang_intervall - $duschvorhang_vergangen;
+    if ($duschvorhang_verbleibend > 1)
+        $duschvorhang_faellig = "in " . $duschvorhang_verbleibend . " Tagen";
+    elseif ($duschvorhang_verbleibend == 1)
+        $duschvorhang_faellig = "morgen";
+    elseif ($duschvorhang_verbleibend == 0)
+        $duschvorhang_faellig = "<span class='due'>heute</span>";
+    elseif ($duschvorhang_verbleibend == -1)
+        $duschvorhang_faellig = "<span class='overdue'>gestern</span>";
+    else
+        $duschvorhang_faellig = "<span class='overdue'>seit " . abs($duschvorhang_verbleibend) . " Tagen</span>"
 ?>
 
 	<table class="putzplan">
@@ -41,21 +102,21 @@
 		<tr>
 		    <th>Küche putzen</th>
 		    <td><?php echo $json['kueche']['dran']; ?></td>
-		    <td></td>
+		    <td><?php echo $kueche_faellig; ?></td>
 		</tr>
 		<tr>
 		    <th>Staubsaugen</th>
 		    <td><?php echo $json['saugen']['dran']; ?></td>
-		    <td></td>
+		    <td><?php echo $saugen_faellig; ?></td>
 		</tr>
 		<tr>
 		    <th>Handtücher waschen</th>
 		    <td><?php echo $json['handtuecher']['dran']; ?></td>
-		    <td></td>
+		    <td><?php echo $handtuecher_faellig; ?></td>
 		</tr>
 		<tr>
 		    <th>Duschvorhänge waschen</th>
 		    <td><?php echo $json['duschvorhang']['dran']; ?></td>
-		    <td></td>
+		    <td><?php echo $duschvorhang_faellig; ?></td>
 		</tr>
 	</table>
