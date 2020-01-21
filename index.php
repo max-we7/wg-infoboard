2810492082c5d25624963914e9003eac9c94216b
Max <mail@max-werner.net>
2020-01-21 18:27:44 +0100

    update

--- index.php
+++ index.php
@@ -13,6 +13,30 @@
   <a href="#" id="refresh_onesecond" hidden="">refresh</a>
   <a href="#" id="refresh_tenminutes" hidden="">refresh</a>
 
+  <div id="newsbanner">
+	<div class="marquee" id="mycrawler">
+			Those confounded friars dully buzz that faltering jay. An appraising tongue acutely causes our courageous hogs. Their fitting submarines deftly break your approving improvisations. Her downcast taxonomies actually box up those disgusted turtles.
+	</div>
+	</div>
+	<script type="text/javascript">
+		marqueeInit({
+			uniqueid: 'mycrawler',
+			style: {
+				'padding': '5px',
+				'width': '100%',
+				<!-- 'font-size': '1.5rem', -->
+				'background': 'black',
+				'color': 'white',
+			},
+			inc: 5, //speed - pixel increment for each iteration of this marquee's movement
+			mouse: 'cursor driven', //mouseover behavior ('pause' 'cursor driven' or false)
+			moveatleast: 2,
+			neutral: 150,
+			persist: true,
+			savedirection: true
+		});
+		</script>
+
 </head>
 	<div id="flexbox">
 		<div class="inner_flexbox">
@@ -41,27 +65,7 @@
 
 //	<marquee>Newsticker: This is a newsticker prototype ;-) ++ Breaking WG News: Bad wurde geputzt ++
 //	  WG Party am kommenden Samstag ++ Coming soon: Abfallkalender ++ Coming soon: ECHTE NEWS ++ </marquee>
-	<div class="marquee" id="mycrawler">
-			Those confounded friars dully buzz that faltering jay. An appraising tongue acutely causes our courageous hogs. Their fitting submarines deftly break your approving improvisations. Her downcast taxonomies actually box up those disgusted turtles.
-	</div>
-	<script type="text/javascript">
-		marqueeInit({
-			uniqueid: 'mycrawler',
-			style: {
-				'padding': '5px',
-				'width': '100%',
-				<!-- 'font-size': '1.5rem', -->
-				'background': 'black',
-				'color': 'white',
-			},
-			inc: 5, //speed - pixel increment for each iteration of this marquee's movement
-			mouse: 'cursor driven', //mouseover behavior ('pause' 'cursor driven' or false)
-			moveatleast: 2,
-			neutral: 150,
-			persist: true,
-			savedirection: true
-		});
-		</script>
+
 
 	<footer>
 		<p>&copy; <?php echo date("Y");?> Designed and Engineered by Max Werner</p>
