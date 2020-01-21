1350e35e8afa19ea5793b02b8e9b54fa5cce1ace
Max <mail@max-werner.net>
2020-01-21 11:08:22 +0100

    updated xkcd source

--- php/xkcd.php
+++ php/xkcd.php
@@ -1,10 +1,14 @@
 <?php
 	# Load from JSON
-//    $str = file_get_contents('../data/xkcd.json');
-//    $json = json_decode($str, true);
-    $array = explode("\n", file_get_contents('../data/xkcd.txt'));
+    $str = file_get_contents('http://xkcd.com/info.0.json');
+    $json = json_decode($str, true);
+    $link = $json["img"]
+//    $array = explode("\n", file_get_contents('../data/xkcd.txt'));
 ?>
 
-
-Today's XKCD
-<img src=<?php echo $array[0];?>/>
+<div>
+    <p>Today's XKCD</p>
+</div>
+<div>
+    <img style="height:25%;max-width:40%;" src=<?php echo $link;?>/>
+</div>
