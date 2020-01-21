f69f61721bc759b0d14caea3c500253b68208404
Max <mail@max-werner.net>
2020-01-21 17:58:01 +0100

    update

--- php/muell.php
+++ php/muell.php
@@ -10,28 +10,28 @@
     if ($schwarz > 1)
         $schwarz_faellig = "in " . $schwarz . " Tagen";
     elseif ($schwarz == 1)
-        $schwarz_faellig = "<span class='due'>morge</span>";
+        $schwarz_faellig = "<span class='due'>morgen</span>";
     elseif ($schwarz == 0)
         $schwarz_faellig = "<span class='overdue'>heute</span>";
 
     if ($gelb > 1)
         $gelb_faellig = "in " . $gelb . " Tagen";
     elseif ($gelb == 1)
-        $gelb_faellig = "<span class='due'>morge</span>";
+        $gelb_faellig = "<span class='due'>morgen</span>";
     elseif ($gelb == 0)
         $gelb_faellig = "<span class='overdue'>heute</span>";
 
     if ($blau > 1)
         $blau_faellig = "in " . $blau . " Tagen";
     elseif ($blau == 1)
-        $blau_faellig = "<span class='due'>morge</span>";
+        $blau_faellig = "<span class='due'>morgen</span>";
     elseif ($blau == 0)
         $blau_faellig = "<span class='overdue'>heute</span>";
 
     if ($gruen > 1)
         $gruen_faellig = "in " . $gruen . " Tagen";
     elseif ($gruen == 1)
-        $gruen_faellig = "<span class='due'>morge</span>";
+        $gruen_faellig = "<span class='due'>morgen</span>";
     elseif ($gruen == 0)
         $gruen_faellig = "<span class='overdue'>heute</span>";
 ?>
--- php/xkcd.php
+++ php/xkcd.php
@@ -3,12 +3,11 @@
     $str = file_get_contents('http://xkcd.com/info.0.json');
     $json = json_decode($str, true);
     $link = $json["img"]
-//    $array = explode("\n", file_get_contents('../data/xkcd.txt'));
 ?>
 
 <div>
     <p>Today's XKCD</p>
 </div>
 <div>
-    <img style="height:25%;max-width:40%;" src="<?php echo $link;?>"/>
+    <img src="<?php echo $link;?>"/>
 </div>
