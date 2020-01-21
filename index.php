67a589a2d95e3b3b9b905587f45adaa1a56bf1a0
Max <mail@max-werner.net>
2020-01-21 22:22:42 +0100

    update

--- index.php
+++ index.php
@@ -15,7 +15,7 @@
 
 		<div id="footer">
 		<div class="marquee" id="mycrawler">
-			Those es actted turtles.fffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff
+			Those confounded friars dully buzz that faltering jay. An appraising tongue acutely causes our courageous hogs. Their fitting submarines deftly break your approving improvisations. Her downcast taxonomies actually box up those disgusted turtles.
 		</div>
 		</div>
 
