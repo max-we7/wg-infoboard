b0055f4cc9eac00c14fcb41eb023a5270d2579b5
Max <mail@max-werner.net>
2020-01-21 22:45:11 +0100

    update

--- js/script.js
+++ js/script.js
@@ -8,7 +8,7 @@ $(document).ready(function() {
 	 $("#putzen").load("php/putzplan.php");
 	 $("#xkcd").load("php/news.php");
 	 $("#zaw").load("php/muell.php");
-	 $("#hidemyass").load("php/news.php");
+	 $("#testtest").load("php/news.php");
 
     return false;
     });
--- php/news.php
+++ php/news.php
@@ -2,11 +2,13 @@
 	# Load from JSON
     $str = file_get_contents('../data/news.json');
     $json = json_decode($str, true);
-
+    $var = ""
 ?>
 
 <div id="news">
     <?php foreach($json as $item) {?>
-		+++ <?php echo $item;?>
+		<?php $var = $var . $item;?>
 	<?php }	?>
 </div>
+
+<?php echo $var;?>
