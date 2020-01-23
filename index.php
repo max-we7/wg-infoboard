dfaca0b16b10d92431628d2f1990dd7cd19ebc97
Max <mail@max-werner.net>
2020-01-23 08:58:05 +0100

    update

--- index.php
+++ index.php
@@ -25,9 +25,8 @@
       var marquee = window.m = new dynamicMarquee.Marquee($marquee, { rate: -100 });
 
 
-	  var obj = JSON.parse(testy);
       window.l = dynamicMarquee.loop(marquee, [
-        function() { return obj.name; },
+        function() { return 'hello test'; },
         function() { return 'It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old'; },
         function() { return 'Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the morrce'; },
         function() { return 'RLorem Ipsum comescero, written in 45 BC'; }
