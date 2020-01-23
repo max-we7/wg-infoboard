b63a99eabbc858a98ebbd3993759c2ed1c44611a
Max <mail@max-werner.net>
2020-01-23 09:00:10 +0100

    update

--- index.php
+++ index.php
@@ -20,7 +20,22 @@
 			<div id="marquee"></div>
 		    <div id="test">
 
+		    <script type="text/javascript">
+      var $marquee = document.getElementById('marquee');
+      var marquee = window.m = new dynamicMarquee.Marquee($marquee, { rate: -100 });
 
+
+      window.l = dynamicMarquee.loop(marquee, [
+        function() { return 'hello test'; },
+        function() { return 'It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old'; },
+        function() { return 'Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the morrce'; },
+        function() { return 'RLorem Ipsum comescero, written in 45 BC'; }
+      ], function() {
+        var $separator = document.createElement('div');
+        $separator.innerHTML = '&nbsp|&nbsp';
+        return $separator;
+      });
+	  </script>
 
 
 		    </div>
--- php/news.php
+++ php/news.php
@@ -3,9 +3,8 @@
       var marquee = window.m = new dynamicMarquee.Marquee($marquee, { rate: -100 });
 
 
-	  var obj = JSON.parse(testy);
       window.l = dynamicMarquee.loop(marquee, [
-        function() { return obj.name; },
+        function() { return 'it works' },
         function() { return 'It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old'; },
         function() { return 'Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the morrce'; },
         function() { return 'RLorem Ipsum comescero, written in 45 BC'; }
