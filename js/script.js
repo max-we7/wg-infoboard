a716303f21e36af28f16b2d732662646c8cc4113
Max <mail@max-werner.net>
2020-01-23 09:02:46 +0100

    update

--- js/script.js
+++ js/script.js
@@ -8,6 +8,7 @@ $(document).ready(function() {
 	 $("#putzen").load("php/putzplan.php");
 	 $("#xkcd").load("php/xkcd.php");
 	 $("#zaw").load("php/muell.php");
+	 $("#test").empty();
 	 $("#test").load("php/news.php");
 
 
