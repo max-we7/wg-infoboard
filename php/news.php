43c46b6017470e6bf5beca1883c2c9d2cd00ee63
Max <mail@max-werner.net>
2020-01-25 00:25:37 +0100

    update

--- php/news.php
+++ php/news.php
@@ -1,12 +1,6 @@
-<?php
-	# Load from JSON
-    $str = file_get_contents('../data/news.json');
-    $json = json_decode($str, true);
-
-?>
-
-<div id="news">
-    <?php foreach($json as $item) {?>
-		+++ <?php echo $item;?>
-	<?php }	?>
-</div>
+kkkkkkkkkkkkkkkkkkkkkkkkkkkkkllllllllllllllllllllllllllllllllllllllmmmmmmmmmmmmmmmmmmmmmmmmmmm
+nnnnnnnnnnnnnnnnnnnnnnnooooooooooooooooooooooooooooooppppppppppppppppppppppppppppppppppppppppp
+qqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrr
+sssssssssssssssssssssssssssssssssssssssssttttttttttttttttttttttttttttttttttttttttttttttttttt
+uuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvwwwwwwwwwwwwwwwwwwwww
+xxxxxxxxxxxxxxxxxxxxxxxxxxxxxyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyzzzzzzzzzzzzzzzzzzzzzzzzzzzzzz
