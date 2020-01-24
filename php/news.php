38dacb24bc95950362a0367a571a4d8ea21a015f
Max <mail@max-werner.net>
2020-01-25 00:19:19 +0100

    update

--- php/news.php
+++ php/news.php
@@ -1,3 +1,12 @@
-<p>Davos: Robert Habeck nennt Donald Trumps Rede ein Desaster - ZEIT ONLINE</p>
-<p>Impeachment: Donald Trumps Sensenmann im Senat \u2013 McConnell legt Plan vor - t-online.de</p>
-<p>Gleicher Preis f\u00fcr weniger Ware: Mir\u00e1coli ist</p>
+<?php
+	# Load from JSON
+    $str = file_get_contents('../data/news.json');
+    $json = json_decode($str, true);
+
+?>
+
+<div id="news">
+    <?php foreach($json as $item) {?>
+		+++ <?php echo $item;?>
+	<?php }	?>
+</div>
