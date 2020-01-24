c38b8ab03af743ed6d8f54d114430bc469bd90ec
Max <mail@max-werner.net>
2020-01-24 20:42:10 +0100

    update

--- index.php
+++ index.php
@@ -14,46 +14,30 @@
   <a href="#" id="refresh_onesecond" hidden="">refresh</a>
   <a href="#" id="refresh_tenminutes" hidden="">refresh</a>
 
-		<div id="footer">
-		<marquee>
-			Newsticker: This is a newsticker prototype ;-) ++ Breaking WG News: Bad wurde geputzt ++
-	        WG Party am kommenden Samstag ++ Coming soon: ECHTE NEWS ++
-        </marquee>
-		</div>
-
-
 
 </head>
-	<div id="flexbox">
-		<div class="inner_flexbox">
-			<div id="nahverkehr"></div>
-			<div id="regionalverkehr"></div>
-		</div> <!-- inner_flexbox -->
-		<div class="inner_flexbox">
-			<div id="clock"></div>
-			<div id="weather">
-			    <div id="wcom-82d5e81c8529ca7eab7a4b2c15ecefd4" class="wcom-default w300x250" style="border: 0px none; background-color: rgb(0, 0, 0); border-radius: 5px; color: rgb(255, 255, 255);"><link rel="stylesheet" href="//cs3.wettercomassets.com/woys/5/css/w.css" media="all"><div class="wcom-city"><a style="color: rgb(255, 255, 255);" href="https://www.wetter.com/deutschland/griesheim/DE0003699.html" target="_blank" rel="nofollow" aria-label="Wetter Berlin" title="Wetter Griesheim">Wetter Griesheim</a></div><div id="wcom-82d5e81c8529ca7eab7a4b2c15ecefd4-weather"></div><script type="text/javascript" src="//cs3.wettercomassets.com/woys/5/js/w.js"></script><script type="text/javascript">_wcomWidget({id: 'wcom-82d5e81c8529ca7eab7a4b2c15ecefd4',location: 'DE0003699',format: '300x250',type: 'spaces'});</script></div>
-			</div> <!-- weather -->
-		</div> <!-- inner_flexbox -->
-		<div class="inner_flexbox">
-			<div id="einkaufen"></div>
-		</div> <!-- inner_flexbox -->
-		<div class="inner_flexbox">
-		    <div id="putzen"></div>
-		</div> <!-- inner_flexbox -->
-		<div class="inner_flexbox">
-            <div id="xkcd"></div>
-		</div> <!-- inner_flexbox -->
-		<div class="inner_flexbox">
-            <div id="zaw"></div>
-		</div> <!-- inner_flexbox -->
-	</div> <!-- flexbox -->
+	<div class="box" id="einseins"></div>
+	<div class="box" id="einszwei"></div>
+
+	<div class="box" id="zweieins"></div>
+	<div class="box" id="zweizwei"></div>
+	<div class="box" id="zweidrei"></div>
+
+	<div class="box" id="dreieins"></div>
+	<div class="box" id="dreizwei"></div>
+	<div class="box" id="dreidrei"></div>
+	<div class="box" id="dreivier"></div>
 
 
+	<marquee>
+	<p>This text is FLYING</p>
+	</marquee>
 
 	<footer>
-		<p>&copy; <?php echo date("Y");?> Designed and Engineered by Max Werner</p>
+	<p>&copy; 2020 Designed and Engineered by Max Werner</p>
+
 	</footer>
+
 
 </body>
 </html>
