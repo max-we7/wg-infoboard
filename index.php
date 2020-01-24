3fca59aaf649009f1af39188bc753e356166d605
Max <mail@max-werner.net>
2020-01-24 23:03:18 +0100

    update

--- index.php
+++ index.php
@@ -35,7 +35,11 @@
 	<div class="box" id="dreieins">
 	    <div id="putzen"></div>
 	</div>
-	<div class="box" id="dreizwei"></div>
+	<div class="box" id="dreizwei">
+	    <div id="weather">
+	        <div id="wcom-82d5e81c8529ca7eab7a4b2c15ecefd4" class="wcom-default w300x250" style="border: 0px none; background-color: rgb(0, 0, 0); border-radius: 5px; color: rgb(255, 255, 255);"><link rel="stylesheet" href="//cs3.wettercomassets.com/woys/5/css/w.css" media="all"><div class="wcom-city"><a style="color: rgb(255, 255, 255);" href="https://www.wetter.com/deutschland/griesheim/DE0003699.html" target="_blank" rel="nofollow" aria-label="Wetter Berlin" title="Wetter Griesheim">Wetter Griesheim</a></div><div id="wcom-82d5e81c8529ca7eab7a4b2c15ecefd4-weather"></div><script type="text/javascript" src="//cs3.wettercomassets.com/woys/5/js/w.js"></script><script type="text/javascript">_wcomWidget({id: 'wcom-82d5e81c8529ca7eab7a4b2c15ecefd4',location: 'DE0003699',format: '300x250',type: 'spaces'});</script></div>
+	    </div>
+	</div>
 	<div class="box" id="dreidrei"></div>
 
 
