e3c4e9a8b9c1422d043c02fe7298599e42598f0d
Max <mail@max-werner.net>
2020-01-25 10:29:29 +0100

    update

--- php/news.php
+++ php/news.php
@@ -1,7 +1,13 @@
-+++ Coronavirus (Lungenkrankheit): Alarmzustand in China - Millionenmetropole abgeschottet | Welt - merkur.de +++
-Weltwirtschaftsforum Davos: USA und Frankreich legen Streit \u00fcber Digitalsteuer bei - t-online.de +++
-Streit um \u201eDiese eG\u201c im Hauptausschuss: Florian Schmidt sieht sich weiterhin im Recht - Tagesspiegel +++
-Weltwirtschaftsforum - Handelsstreit auf offener B\u00fchne - S\u00fcddeutsche Zeitung +++
-Wetter in Deutschland: Dieser Teil des Landes darf auf Sonne hoffen - t-online.de +++
-Beh\u00f6rde verrei\u00dft Heils Konzept:Rentenversicherung meutert bei Grundrente - n-tv NACHRICHTEN +++
-Einigung im Streit \u00fcber Luftschadstoffe: Tempo 30 statt Fahrverbote - DER SPIEGEL +++
+
+<?php
+	# Load from JSON
+    $str = file_get_contents('../data/news.json');
+    $json = json_decode($str, true);
+
+?>
+
+<div id="news">
+    <?php foreach($json as $item) {?>
+		+++ <?php echo $item;?>
+	<?php }	?>
+</div>
