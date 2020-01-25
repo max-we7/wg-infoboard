6440f64486f03b598e20b8053d5efc727103ce76
Max <mail@max-werner.net>
2020-01-25 11:35:58 +0100

    update

--- php/clock.php
+++ php/clock.php
@@ -3,7 +3,7 @@
 	date_default_timezone_set('Europe/Berlin');
 	setlocale(LC_ALL, 'de_DE');
 	$time = date('H:i');
-	$date = date('d. M Y');
+	$date = date('d. m Y');
 	//$days = array('Sonntag', 'Montag', 'Dienstag', 'Mittwoch','Donnerstag','Freitag', 'Samstag');
 	//$day_of_week_int = date('w');
 	//$day_of_week = $days[$day_of_week_int];
