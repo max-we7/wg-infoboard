2cfecffb5ca2aefb15cac78685e3cb0dae1ef255
Max <mail@max-werner.net>
2020-01-27 22:31:12 +0100

    update

--- telegram-bot/gif_handler.py
+++ telegram-bot/gif_handler.py
@@ -1,6 +1,14 @@
+import ffmpy
+
+
 def handle_gif(msg, bot):
     print("starting download")
-    bot.download_file(msg['document']['file_id'], '../data/gif.mp4')
+    bot.download_file(msg['document']['file_id'], '../data/video.mp4')
+    ff = ffmpy.FFmpeg(
+        inputs={"video.mp4": None},
+        outputs={"animation.gif": None})
+
+    ff.run
     print("downloaded")
     pass
 
