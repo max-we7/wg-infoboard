f46a96fe6cdb678963a108cade091d91e9f01bbe
Max <mail@max-werner.net>
2020-02-02 15:42:22 +0100

    update

--- telegram-bot/gif_handler.py
+++ telegram-bot/gif_handler.py
@@ -4,12 +4,10 @@ import sys
 
 
 def handle_gif(msg, bot):
-    print("starting download")
-    bot.sendMessage(msg['chat']['id'], "starting download")
     bot.download_file(msg['document']['file_id'], '../data/video.mp4')
+    bot.sendMessage(msg['chat']['id'], "downloading... done")
     convert_to_gif(msg, bot)
-    bot.sendMessage(msg['chat']['id'], "downloaded")
-    print("downloaded")
+    bot.sendMessage(msg['chat']['id'], "done.")
 
 
 def handle_img(msg, bot):
@@ -21,7 +19,7 @@ def convert_to_gif(msg, bot):
         inputpath = "../data/video.mp4"
         target_format = ".gif"
         outputpath = os.path.splitext(inputpath)[0] + target_format
-        print("converting\r\n\t{0}\r\nto\r\n\t{1}".format(inputpath, outputpath))
+        bot.sendMessage(msg['chat']['id'], "converting to GIF... please wait...")
 
         reader = imageio.get_reader(inputpath)
         fps = reader.get_meta_data()['fps']
@@ -31,8 +29,6 @@ def convert_to_gif(msg, bot):
             sys.stdout.write("\rframe {0}".format(i))
             sys.stdout.flush()
             writer.append_data(im)
-        print("\r\nFinalizing...")
         writer.close()
-        print("Done.")
     except Exception as e:
         bot.sendMessage(msg['chat']['id'], "error converting GIF")
