7d59136f7e86d006aaf16bbb7692baea18ad14b3
Max <mail@max-werner.net>
2020-02-02 16:55:16 +0100

    update

--- telegram-bot/gif_handler.py
+++ telegram-bot/gif_handler.py
@@ -8,12 +8,13 @@ import platform
 
 def handle_gif(msg, bot):
     bot.download_file(msg['document']['file_id'], '../data/video.mp4')
-    bot.sendMessage(msg['chat']['id'], "downloading... done")
+    bot.sendMessage(msg['chat']['id'], "downloading file... done!")
     convert_to_gif(msg, bot)
     if platform.system() == "Linux":
         subprocess.run(["sudo", "service", "kiosk.sh", "restart"])
-        bot.sendMessage(msg['chat']['id'], "flushing browser cache...")
-    bot.sendMessage(msg['chat']['id'], "done.")
+        bot.sendMessage(msg['chat']['id'], "flushing browser cache... done!")
+    else:
+        bot.sendMessage(msg['chat']['id'], "cache flushing failed.")
 
 
 def handle_img(msg, bot):
@@ -25,8 +26,7 @@ def convert_to_gif(msg, bot):
         inputpath = "../data/video.mp4"
         target_format = ".gif"
         outputpath = os.path.splitext(inputpath)[0] + target_format
-        bot.sendMessage(msg['chat']['id'], "converting to GIF... please wait...")
-        sent_message = bot.sendMessage(msg['chat']['id'], "starting...")
+        sent_message = bot.sendMessage(msg['chat']['id'], "starting conversion to GIF...")
         message_id = telepot.message_identifier(sent_message)
 
         reader = imageio.get_reader(inputpath)
@@ -37,7 +37,7 @@ def convert_to_gif(msg, bot):
         for j in enumerate(reader):
             total_frames += 1
         for i, im in enumerate(reader):
-            bot.editMessageText(message_id, f"Converting Frame {i} of {total_frames}...")
+            bot.editMessageText(message_id, f"Converting to GIF: Frame {i} of {total_frames}...")
             sys.stdout.write("\rframe {0}".format(i))
             sys.stdout.flush()
             writer.append_data(im)
