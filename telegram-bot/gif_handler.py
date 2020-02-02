87dec049603642b32aa993c313de7417152ca0fd
Max <mail@max-werner.net>
2020-02-02 15:16:21 +0100

    update

--- telegram-bot/gif_handler.py
+++ telegram-bot/gif_handler.py
@@ -7,7 +7,7 @@ def handle_gif(msg, bot):
     print("starting download")
     bot.sendMessage(msg['chat']['id'], "starting download")
     bot.download_file(msg['document']['file_id'], '../data/video.mp4')
-    convert_to_gif()
+    convert_to_gif(msg, bot)
     bot.sendMessage(msg['chat']['id'], "downloaded")
     print("downloaded")
 
@@ -16,20 +16,23 @@ def handle_img(msg, bot):
     pass
 
 
-def convert_to_gif():
-    inputpath = "../data/video.mp4"
-    target_format = ".gif"
-    outputpath = os.path.splitext(inputpath)[0] + target_format
-    print("converting\r\n\t{0}\r\nto\r\n\t{1}".format(inputpath, outputpath))
+def convert_to_gif(msg, bot):
+    try:
+        inputpath = "../data/video.mp4"
+        target_format = ".gif"
+        outputpath = os.path.splitext(inputpath)[0] + target_format
+        print("converting\r\n\t{0}\r\nto\r\n\t{1}".format(inputpath, outputpath))
 
-    reader = imageio.get_reader(inputpath)
-    fps = reader.get_meta_data()['fps']
+        reader = imageio.get_reader(inputpath)
+        fps = reader.get_meta_data()['fps']
 
-    writer = imageio.get_writer(outputpath, fps=fps)
-    for i, im in enumerate(reader):
-        sys.stdout.write("\rframe {0}".format(i))
-        sys.stdout.flush()
-        writer.append_data(im)
-    print("\r\nFinalizing...")
-    writer.close()
-    print("Done.")
+        writer = imageio.get_writer(outputpath, fps=fps)
+        for i, im in enumerate(reader):
+            sys.stdout.write("\rframe {0}".format(i))
+            sys.stdout.flush()
+            writer.append_data(im)
+        print("\r\nFinalizing...")
+        writer.close()
+        print("Done.")
+    except Exception as e:
+        bot.sendMessage(msg['chat']['id'], "error converting GIF")
