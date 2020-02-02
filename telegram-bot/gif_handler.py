102e24a73c0607d544a7935b37b0c214dd536da3
Max <mail@max-werner.net>
2020-02-02 14:55:55 +0100

    update

--- telegram-bot/gif_handler.py
+++ telegram-bot/gif_handler.py
@@ -1,6 +1,6 @@
-# import imageio
-# import os
-# import sys
+import imageio
+import os
+import sys
 
 
 def handle_gif(msg, bot):
@@ -14,20 +14,20 @@ def handle_img(msg, bot):
     pass
 
 
-# def convert_to_gif():
-#     inputpath = "../data/video.mp4"
-#     target_format = ".gif"
-#     outputpath = os.path.splitext(inputpath)[0] + target_format
-#     print("converting\r\n\t{0}\r\nto\r\n\t{1}".format(inputpath, outputpath))
-#
-#     reader = imageio.get_reader(inputpath)
-#     fps = reader.get_meta_data()['fps']
-#
-#     writer = imageio.get_writer(outputpath, fps=fps)
-#     for i, im in enumerate(reader):
-#         sys.stdout.write("\rframe {0}".format(i))
-#         sys.stdout.flush()
-#         writer.append_data(im)
-#     print("\r\nFinalizing...")
-#     writer.close()
-#     print("Done.")
+def convert_to_gif():
+    inputpath = "../data/video.mp4"
+    target_format = ".gif"
+    outputpath = os.path.splitext(inputpath)[0] + target_format
+    print("converting\r\n\t{0}\r\nto\r\n\t{1}".format(inputpath, outputpath))
+
+    reader = imageio.get_reader(inputpath)
+    fps = reader.get_meta_data()['fps']
+
+    writer = imageio.get_writer(outputpath, fps=fps)
+    for i, im in enumerate(reader):
+        sys.stdout.write("\rframe {0}".format(i))
+        sys.stdout.flush()
+        writer.append_data(im)
+    print("\r\nFinalizing...")
+    writer.close()
+    print("Done.")
