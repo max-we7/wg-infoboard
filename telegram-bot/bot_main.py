b0ed4ce7f6d5159eab2ec0bac550d991964948f3
Max <mail@max-werner.net>
2020-02-04 16:46:56 +0100

    update

--- telegram-bot/bot_main.py
+++ telegram-bot/bot_main.py
@@ -16,54 +16,35 @@ bot = telepot.Bot(API_KEY)
 
 def handle(msg):
     """
-    handles all incoming messages
+    handle all incoming messages
     """
-    print(msg)
+    print(msg)  # print all incoming messages for easy debugging
     content_type, chat_type, chat_id = telepot.glance(msg)
-    # uncomment this to identify new Users
-    # if str(chat_id) not in LEGIT_IDS:
-    #     bot.sendMessage(msg['chat']['id'], f"It seems you do not have access rights to this bot.\n\nYour Telegram ID "
-    #                                        f"is: <b>{msg['from']['id']}</b>.\n\nUse it to ask for permission to use "
-    #                                        f"this bot.", parse_mode="html")
-    try:
-        if msg['animation']:
-            print("Animation detected!")
-            handle_gif(msg, bot)
-    except KeyError:
-        pass
-    try:
-        if msg['photo']:
-            print("Image detected!")
-            # print(msg['photo'][0]['file_id'])
-            handle_img(msg, bot)
-    except KeyError:
-        pass
-    if content_type == 'text' and chat_type == "private":
-        if str(chat_id) in LEGIT_IDS:
-            choose_command(bot, msg)
-        else:
-            logging.info(f"Not in legit IDs: {msg['chat']['first_name']}, {msg['chat']['id']}")
-    elif content_type == 'text' and chat_type == "group":
+    if str(chat_id) in LEGIT_IDS:
         try:
-            if msg['entities'][0]['type'] == 'mention' or msg['entities'][0]['type'] == 'bot_command':
-                if str(chat_id) in LEGIT_IDS:
-                    choose_command(bot, msg)
-                else:
-                    logging.info(f"Not in legit IDs: {msg['chat']['title']}, {msg['chat']['id']}")
+            if msg['animation']:
+                handle_gif(msg, bot)
         except KeyError:
             pass
-    elif str(chat_id) not in LEGIT_IDS:
+        try:
+            if msg['photo']:
+                handle_img(msg, bot)
+        except KeyError:
+            pass
+        if content_type == 'text':
+            choose_command(bot, msg)
+        else:
+            logging.debug(f"Invalid input given: {msg}")
+    else:
         bot.sendMessage(msg['chat']['id'], f"It seems you do not have access rights to this bot.\n\nYour Telegram ID "
                                            f"is: <b>{msg['from']['id']}</b>.\n\nUse it to ask for permission to use "
                                            f"this bot.", parse_mode="html")
-    else:
-        logging.debug("Invalid input given")
+        logging.info(f"Not in legit IDs: {msg['from']['first_name']}, {msg['from']['id']}")
 
 
 try:
     MessageLoop(bot, handle).run_as_thread()
     schedule.every().day.at("00:02").do(update_putzplan)
-
     while True:
         schedule.run_pending()
         time.sleep(10)
