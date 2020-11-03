32a294617276577c17d3d6ec9e2e75d8e34bbf02
Max <mail@max-werner.net>
2020-11-03 15:35:40 +0100

    added script to measure DHT22

--- php/putzplan.php
+++ php/putzplan.php
@@ -137,7 +137,7 @@
 	</table>
 
 <?php $cpu_temp = file_get_contents('../data/cpu_temp.txt'); ?>
-<?php echo "CPU Temperatur: $cpu_temp";?>
+<?php echo "CPU Temperatur: $cpu_temp  |  ";?>
 <?php $temp = file_get_contents('../data/temp.txt'); ?>
 <?php echo $temp;?>
 
