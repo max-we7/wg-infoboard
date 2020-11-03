f0ce418fb3413b602f993f0849568cc77ccd4664
Max <mail@max-werner.net>
2020-11-03 15:25:12 +0100

    added script to measure DHT22

---
+++ python/measure_temp.py
@@ -0,0 +1,25 @@
+import adafruit_dht
+import board
+import time
+dhtDevice = adafruit_dht.DHT22(board.D18, use_pulseio=False)
+
+while True:
+    try:
+        # Print the values to the serial port
+        temperature_c = dhtDevice.temperature
+        humidity = dhtDevice.humidity
+        output = "Temp: {:.1f} C    Humidity: {}% ".format(temperature_c, humidity)
+        print(output)
+        with open("temp.txt", "w") as f:
+            f.write(output)
+
+    except RuntimeError as error:
+        # Errors happen fairly often, DHT's are hard to read, just keep going
+        print(error.args[0])
+        time.sleep(2.0)
+        continue
+    except Exception as error:
+        dhtDevice.exit()
+        raise error
+
+    time.sleep(1.0)
