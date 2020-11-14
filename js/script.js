19ed44712d118a4882a21e57deb026e1bbfc0626
max <mail@max-werner.net>
2020-11-14 23:01:19 +0100

    update

--- js/script.js
+++ js/script.js
@@ -118,8 +118,8 @@ function updateGarbage(){
         }
 
         function blink(selector){
-            $(selector).fadeOut('1000', function(){
-                $(this).fadeIn('1000', function(){
+            $(selector).fadeOut(1000, function(){
+                $(this).fadeIn(1000, function(){
                     blink(this);
                 });
             });
