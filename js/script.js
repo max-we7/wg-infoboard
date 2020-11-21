0889c505f72e7cf56e7143a9de7bdaadc03088cf
Max <mail@max-werner.net>
2020-11-21 17:13:32 +0100

    weather widget

--- js/script.js
+++ js/script.js
@@ -213,5 +213,5 @@ function updateTrainSchedule(){
 }
 
 function changeWeatherTheme(){
-    $("weatherwidget-io").css("data-theme", "dark");
+    $('.weatherwidget-io').css("data-theme", "dark");
 }
