140f66a6eaf2302cde296dee9caadd3972512573
Max <mail@max-werner.net>
2020-11-21 20:37:21 +0100

    weather widget

--- js/script.js
+++ js/script.js
@@ -232,6 +232,9 @@ function initializeWeather() {
      if (50 <= currentTime&&currentTime <= 59) {
          document.getElementById("weatherwid").setAttribute("data-theme", "orange");
      }
-     !function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src='https://weatherwidget.io/js/widget.min.js';fjs.parentNode.insertBefore(js,fjs);}}(document,'script','weatherwidget-io-js');
+     wid(document,'script','weatherwidget-io-js');
 }
 
+function wid(d,s,id){
+    var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src='https://weatherwidget.io/js/widget.min.js';fjs.parentNode.insertBefore(js,fjs);}
+}
