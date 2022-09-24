1deb615dac07ad55ed713bb6ce042d4980b3c2c6
Max <mail@max-werner.net>
2022-09-24 20:45:52 +0200

    send DHT11 values to MQTT Broker

--- python/measure_temp.py
+++ python/measure_temp.py
@@ -1,7 +1,36 @@
 import adafruit_dht
 import board
 import time
+import datetime
+import paho.mqtt.client as mqttClient
 dhtDevice = adafruit_dht.DHT22(board.D18, use_pulseio=False)
+
+
+def on_connect(client, userdata, flags, rc):
+    if rc == 0:
+
+        print("Connected to broker")
+
+        global Connected  # Use global variable
+        Connected = True  # Signal connection
+
+    else:
+
+        print("Connection failed")
+
+
+broker_address = "192.168.2.39"
+port = 1617
+user = "mqtt_user"
+password = "TiK71xS/G07iO6neX&mA"
+Connected = False
+
+client = mqttClient.Client("DHT_Kueche")
+client.username_pw_set(user, password=password)
+client.on_connect = on_connect
+client.connect(broker_address, port=port)
+client.loop_start()
+
 
 while True:
     try:
@@ -10,6 +39,11 @@ while True:
         humidity = dhtDevice.humidity
         output = "Raumtemperatur: {:.1f}°C  |  Luftfeuchtigkeit: {}% ".format(temperature_c, humidity)
         print(output)
+        if Connected:
+            print("sending values:")
+            client.publish("DHT11_Kueche/time_updated", f"{datetime.datetime.now()}")
+            client.publish("DHT11_Kueche/temperature", "{:.1f}°C".format(temperature_c))
+            client.publish("DHT11_Kueche/humidity", "{}% ".format(humidity))
         with open("../data/temp.txt", "w") as f:
             f.write(output)
 
@@ -21,5 +55,8 @@ while True:
     except Exception as error:
         dhtDevice.exit()
         raise error
+    except KeyboardInterrupt:
+        client.disconnect()
+        client.loop_stop()
 
     time.sleep(1.0)
