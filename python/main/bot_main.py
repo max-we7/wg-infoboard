a283d97c044c0d78aef523efe85275b967e24fbb
Max <mail@max-werner.net>
2022-12-31 00:19:40 +0100

    run garbage script on startup

--- python/main/bot_main.py
+++ python/main/bot_main.py
@@ -169,19 +169,20 @@ try:
     # Define Scheduled Jobs
     # noinspection PyBroadException
     try:
-        # schedule.every().day.at("00:01").do(update_putzplan)
-        schedule.every().day.at("19:00").do(update_muell)
+        update_muell()
+
         schedule.every().day.at("00:02").do(update_muell)
         schedule.every().day.at("00:08").do(update_muell)
         schedule.every().day.at("06:02").do(update_muell)
         schedule.every().day.at("07:02").do(update_muell)
         schedule.every().day.at("08:02").do(update_muell)
-        schedule.every().day.at("09:02").do(update_muell)
         schedule.every().day.at("10:02").do(update_muell)
-        schedule.every().day.at("11:02").do(update_muell)
         schedule.every().day.at("12:02").do(update_muell)
-        schedule.every().day.at("13:02").do(update_muell)
         schedule.every().day.at("14:02").do(update_muell)
+        schedule.every().day.at("16:02").do(update_muell)
+        schedule.every().day.at("18:02").do(update_muell)
+        schedule.every().day.at("20:02").do(update_muell)
+        schedule.every().day.at("22:02").do(update_muell)
         # schedule.every().day.at("20:30").do(check_muell_due)
         schedule.every(4).minutes.do(update_infoboard_bahn)
         # TODO: run news updating with schedule instead of cronjob
